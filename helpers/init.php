db4bad3038c7a3e8313363a60d3e55983d1c9cd2
Meet Thakar <mvthakar@outlook.com>
2023-02-09 10:15:37 +0530

    Added IsVerified in getLoggedInUser( )

--- helpers/init.php
+++ helpers/init.php
@@ -84,7 +84,15 @@ function mustBeLoggedIn()
 function getLoggedInUser()
 {
     $request = getRequestBody();
-    return selectOne("SELECT `Email`, `UserRoleId` FROM `Users` WHERE `Id` = ?", [$request->userId]);
+    $user = selectOne("SELECT `Id`, `Email`, `IsVerified`, `UserRoleId` FROM `Users` WHERE `Id` = ?", [$request->userId]);
+    
+    if ($user == null)
+    {
+        http_response_code(403);
+        exit();
+    }
+
+    return $user;
 }
 
 header("Access-Control-Allow-Origin: *");
